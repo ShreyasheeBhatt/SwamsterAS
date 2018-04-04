ad7366c541cfb779cf35f0b96b603f6838cf7c11
epalumbo06 <epalumbo@wpi.edu>
2018-04-04 13:55:48 -0400

    Put the Java files in from Eclipse and started the home page. Made a xml for the coach and swimmer button.

---
+++ app/src/main/java/com/example/shreya/swamsteras/SwamsterEclipse/swimmerandcoach.java
@@ -0,0 +1,18 @@
+package com.example.shreya.swamsteras.SwamsterEclipse;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.shreya.swamsteras.R;
+
+/**
+ * Created by Eliana on 4/3/2018.
+ */
+
+public class swimmerandcoach extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.swimmerandcoachanswer);}
+}
