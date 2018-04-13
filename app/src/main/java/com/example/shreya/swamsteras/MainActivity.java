8bcef8bdfef47d1ce9f5bcf7ae7d5c71ea8b8662
epalumbo06 <epalumbo@wpi.edu>
2018-04-12 20:03:34 -0400

    Added a second page with drop downs that will eventually store the data to create the swimmer number.

--- app/src/main/java/com/example/shreya/swamsteras/MainActivity.java
+++ app/src/main/java/com/example/shreya/swamsteras/MainActivity.java
@@ -4,9 +4,6 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-
-import com.example.shreya.swamsteras.SwamsterEclipse.swimmerandcoach;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);}
 
-    public void onClick(View v) {
-            startActivity(new Intent(MainActivity.this, swimmerandcoach.class));
-        }
+//    public void onClick(View v) {
+//            startActivity(new Intent(MainActivity.this, swimmerandcoach.class));
+//        }
 }
