e84453d2bd1f616b7cac8ed14a219c660107cf17
Shreyashee Bhattacharjee <shreyasheeb@gmail.com>
2018-04-18 10:38:18 -0400

    I changed some stuff in the activity_swim_and_coach_ans.xml file, and I started trying to make a button to go from one activity to another.

--- app/src/main/java/com/example/shreya/swamsteras/MainActivity.java
+++ app/src/main/java/com/example/shreya/swamsteras/MainActivity.java
@@ -4,15 +4,18 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);}
+        setContentView(R.layout.activity_main);
+    }
 
-//    public void onClick(View v) {
-//            startActivity(new Intent(MainActivity.this, swimmerandcoach.class));
-//        }
+    public void onClick(Button doneButton) {
+        Intent intent = new Intent(this, SwimAndCoachAns.class);
+        startActivity(intent);
+    }
 }
--- app/src/main/java/com/example/shreya/swamsteras/SwimAndCoachAns.java
+++ app/src/main/java/com/example/shreya/swamsteras/SwimAndCoachAns.java
@@ -1,13 +1,13 @@
 package com.example.shreya.swamsteras;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Adapter;
-import android.widget.AdapterView;
+import android.util.Log;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.Toast;
+import android.widget.TextView;
 
 public class SwimAndCoachAns extends AppCompatActivity {
 
@@ -16,21 +16,27 @@ public class SwimAndCoachAns extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_swim_and_coach_ans);
 
-    Spinner dropdown = findViewById(R.id.spinner1);
+    Spinner dropdown = findViewById(R.id.meetChoices);
     ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.schools, android.R.layout.simple_spinner_item);
     adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
     dropdown.setAdapter(adapter);
 //    dropdown.setOnItemSelectedListener(this);
 
-    Spinner dropdown2 = findViewById(R.id.spinner2);
+    Spinner dropdown2 = findViewById(R.id.teamChoices);
     ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this, R.array.teams, android.R.layout.simple_spinner_item);
     adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
     dropdown2.setAdapter(adapter2);
 
-    Spinner dropdown3 = findViewById(R.id.spinner3);
+    Spinner dropdown3 = findViewById(R.id.ageChoices);
     ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(this, R.array.ages, android.R.layout.simple_spinner_item);
     adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
     dropdown3.setAdapter(adapter);
+
+    TextView first = findViewById(R.id.firstText);
+    TextView last = findViewById(R.id.firstText);
+    String firstName = first.getText().toString();
+    String lastName = last.getText().toString();
+
 
 //    dropdown.setAdapter(adapter);
 }
