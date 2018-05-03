f3462835c72786a4e70779a2d220fcc6783d73ae
JavaJudiciary <cbkennedy@wpi.edu>
2018-05-03 11:57:47 -0400

    Added things.

--- app/src/main/java/com/example/shreya/swamsteras/SwimAndCoachAns.java
+++ app/src/main/java/com/example/shreya/swamsteras/SwimAndCoachAns.java
@@ -2,8 +2,20 @@ package com.example.shreya.swamsteras;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class SwimAndCoachAns extends AppCompatActivity {
 
@@ -12,25 +24,55 @@ public class SwimAndCoachAns extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_swim_and_coach_ans);
 
-        Spinner dropdown = findViewById(R.id.meetChoices);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.meets, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        dropdown.setAdapter(adapter);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference teamRef = database.getReference("meet");
 
-        Spinner dropdown2 = findViewById(R.id.teamChoices);
-        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this, R.array.teams, android.R.layout.simple_spinner_item);
-        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        dropdown2.setAdapter(adapter2);
+        ArrayList<String> teams = new ArrayList<>();
+        teams.add("Aria");
+        teams.add("Shreya");
+        teams.add("Ellie");
+        teamRef.setValue(teams);
 
-        Spinner dropdown3 = findViewById(R.id.ageChoices);
-        ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(this, R.array.ages, android.R.layout.simple_spinner_item);
-        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        dropdown3.setAdapter(adapter3);
+        teamRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                ArrayList<String> teamList = (ArrayList<String>) dataSnapshot.getValue();
 
-//        TextView first = findViewById(R.id.firstText);
-//        TextView last = findViewById(R.id.firstText);
-//        String firstName = first.getText().toString();
-//        String lastName = last.getText().toString();
-    }
+                Spinner dropdown2 = findViewById(R.id.teamChoices);
+                ArrayAdapter<String> adapter2 = new ArrayAdapter<>(SwimAndCoachAns.this, android.R.layout.simple_spinner_item, teamList);
+                adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                dropdown2.setAdapter(adapter2);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("Error", "Failed to read value.", error.toException());
+            }
+        });
+
+    Spinner dropdown = findViewById(R.id.meetChoices);
+    ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.meets, android.R.layout.simple_spinner_item);
+    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    dropdown.setAdapter(adapter);
+
+    Spinner dropdown3 = findViewById(R.id.ageChoices);
+    ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(this, R.array.ages, android.R.layout.simple_spinner_item);
+    adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    dropdown3.setAdapter(adapter3);
+
+    TextView first = findViewById(R.id.firstText);
+    TextView last = findViewById(R.id.firstText);
+    String firstName = first.getText().toString();
+    String lastName = last.getText().toString();
+
+//    dropdown.setAdapter(adapter);
+}
+
+//    @Override
+//    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+//        String text = parent.getItemAtPosition(position).toString();
+//        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+//    }
 
 }
