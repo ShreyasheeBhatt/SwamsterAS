2049f85adcee20c9192f4bcd148c87d2c4d5fad6
JavaJudiciary <cbkennedy@wpi.edu>
2018-05-07 13:35:08 -0400

    Added things.

--- app/src/main/java/com/example/shreya/swamsteras/FirebasePopulator.java
+++ app/src/main/java/com/example/shreya/swamsteras/FirebasePopulator.java
@@ -9,5 +9,8 @@ public class FirebasePopulator extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_firebase_populator);
-    }
+
+        }
 }
+
+
