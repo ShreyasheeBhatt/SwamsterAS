f9bf3c924a40e990504f873a8fdd2bb0eaef767a
epalumbo06 <epalumbo@wpi.edu>
2018-05-09 11:07:43 -0400

    The swimmer events list view was created.

--- app/src/main/java/com/example/shreya/swamsteras/EventsSwimmer.java
+++ app/src/main/java/com/example/shreya/swamsteras/EventsSwimmer.java
@@ -2,6 +2,11 @@ package com.example.shreya.swamsteras;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class EventsSwimmer extends AppCompatActivity {
 
@@ -10,6 +15,16 @@ public class EventsSwimmer extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_events_swimmer);
 
+        // for on data change
 
+        final ListView lv = (ListView) findViewById(R.id.listIndEvents);
+        final List<String> list = new ArrayList();
+        list.add("backstroke");
+        list.add("swim");
+        list.add("red");
+        ArrayAdapter adapter = new ArrayAdapter<String>(lv.getContext(),android.R.layout.simple_list_item_1,list);
+        lv.setAdapter(adapter);
     }
+
+
 }
