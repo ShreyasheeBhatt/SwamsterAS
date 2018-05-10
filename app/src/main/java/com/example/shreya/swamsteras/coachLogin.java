22264c892311cc440a92a349cfdeb70009d4130f
epalumbo06 <epalumbo@wpi.edu>
2018-05-10 10:45:54 -0400

    Coach's list of their swimmers was added.

--- app/src/main/java/com/example/shreya/swamsteras/coachLogin.java
+++ app/src/main/java/com/example/shreya/swamsteras/coachLogin.java
@@ -1,8 +1,11 @@
 package com.example.shreya.swamsteras;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 public class coachLogin extends AppCompatActivity {
@@ -21,5 +24,13 @@ public class coachLogin extends AppCompatActivity {
         ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this, R.array.teams, android.R.layout.simple_spinner_item);
         adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         dropdown2.setAdapter(adapter2);
+
+        final Button findButton = findViewById(R.id.find);
+        findButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), coachSwimmers.class);
+                startActivity(intent);
+            }
+        });
     }
 }
